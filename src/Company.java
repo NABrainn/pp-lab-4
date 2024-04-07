34257104545fac5601ec8a7749c26e832c61cdd3
nabrain <michal2002.60@gmail.com>
2024-04-07 19:25:37 +0200

    zad 4.2

--- src/Company.java
+++ src/Company.java
@@ -4,7 +4,7 @@ import employees.Worker;
 
 public class Company {
     public static void main(String[] args) {
-        Employee employees[] = new Employee[7];
+        Employee Employees[] = new Employee[7];
 
         Manager employeeSix = new Manager("Ben", 1000, 0);
         Employee employeeSeven = new Employee("Harry", 300);
@@ -15,16 +15,16 @@ public class Company {
         Worker employeeEleven = new Worker("Jack", 1000, "Waiter");
         Employee employeeTwelve = new Employee("William", 200);
 
-        employees[0] = employeeSix;
-        employees[1] = employeeSeven;
-        employees[2] = employeeEight;
-        employees[3] = employeeNine;
-        employees[4] = employeeTen;
+        Employees[0] = employeeSix;
+        Employees[1] = employeeSeven;
+        Employees[2] = employeeEight;
+        Employees[3] = employeeNine;
+        Employees[4] = employeeTen;
 
-        employees[5] = employeeEleven;
-        employees[6] = employeeTwelve;
+        Employees[5] = employeeEleven;
+        Employees[6] = employeeTwelve;
 
-        for(Employee employee : employees) {
+        for(Employee employee : Employees) {
             employee.setSalary(employee.getSalary() + 500);
             if(employee instanceof Manager) {
                 ((Manager) employee).setNumberOfSubordinates(7500);
