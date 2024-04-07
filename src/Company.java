c79ed917aba97e441b12a84efb657b9313adc19f
nabrain <michal2002.60@gmail.com>
2024-04-07 19:01:04 +0200

    zad1.1

--- src/Company.java
+++ src/Company.java
@@ -1,6 +1,6 @@
 public class Company {
     public static void main(String[] args) {
-        Employee employees[] = new Employee[5];
+        Employee[] Employees = new Employee[5];
 
         Employee employeeOne = new Employee("Adam", 10000);
         Employee employeeTwo = new Employee("Beth", 20000);
@@ -8,16 +8,16 @@ public class Company {
         Employee employeeFour = new Employee("Mark", 40000);
         Employee employeeFive = new Employee("Chris", 50000);
 
-        employees[0] = employeeOne;
-        employees[1] = employeeTwo;
-        employees[2] = employeeThree;
-        employees[3] = employeeFour;
-        employees[4] = employeeFive;
+        Employees[0] = employeeOne;
+        Employees[1] = employeeTwo;
+        Employees[2] = employeeThree;
+        Employees[3] = employeeFour;
+        Employees[4] = employeeFive;
 
-        System.out.println("Employee[3]: " + employees[3]);
-        System.out.println("Employee[3] salary: " + employees[3].getSalary());
+        System.out.println("Employee[3]: " + employeeFour);
+        employeeFour.setSalary(540);
 
-        for(Employee employee : employees) {
+        for(Employee employee : Employees) {
             System.out.println(employee);
         }
     }
