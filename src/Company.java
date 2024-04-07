4de5e146c4a952ef0f69f62380dc20917094b98e
nabrain <michal2002.60@gmail.com>
2024-04-07 19:30:08 +0200

    zad 4.3

--- src/Company.java
+++ src/Company.java
@@ -6,26 +6,32 @@ public class Company {
     public static void main(String[] args) {
         Employee Employees[] = new Employee[7];
 
-        Manager employeeSix = new Manager("Ben", 1000, 0);
-        Employee employeeSeven = new Employee("Harry", 300);
-        Employee employeeEight = new Employee("Eric", 4500);
-        Employee employeeNine = new Employee("Jimmy", 9000);
-        Employee employeeTen = new Employee("Walter", 5000);
+        Manager employeeOne = new Manager("Ben", 1000, 0);
+        Employee employeeTwo = new Employee("Harry", 300);
+        Employee employeeThree = new Employee("Eric", 4500);
+        Employee employeeFour = new Employee("Jimmy", 9000);
+        Employee employeeFive = new Employee("Walter", 5000);
 
-        Worker employeeEleven = new Worker("Jack", 1000, "Waiter");
-        Employee employeeTwelve = new Employee("William", 200);
+        Worker employeeSix = new Worker("Jack", 1000, "Waiter");
+        Employee employeeSeven = new Employee("William", 200);
 
-        Employees[0] = employeeSix;
-        Employees[1] = employeeSeven;
-        Employees[2] = employeeEight;
-        Employees[3] = employeeNine;
-        Employees[4] = employeeTen;
+        Employees[0] = employeeOne;
+        Employees[1] = employeeTwo;
+        Employees[2] = employeeThree;
+        Employees[3] = employeeFour;
+        Employees[4] = employeeFive;
 
-        Employees[5] = employeeEleven;
-        Employees[6] = employeeTwelve;
+        Employees[5] = employeeSix;
+        Employees[6] = employeeSeven;
+
+        int count = 0;
+
 
         for(Employee employee : Employees) {
-            employee.setSalary(employee.getSalary() + 500);
+            if(!(employee instanceof Manager)) {
+                count++;
+                employee.setSalary(employee.getSalary() + 500);
+            }
             if(employee instanceof Manager) {
                 ((Manager) employee).setNumberOfSubordinates(7500);
                 employee.setSalary(7500);
